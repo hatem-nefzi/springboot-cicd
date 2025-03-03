1d31b247d4f95763488a12428752ced083086210
hatem-nefzi <hatem.nefzibouali@ensi-uma.tn>
2025-03-03 11:04:08 +0100

    txt file

--- src/main/java/com/example/demo/HelloController.java
+++ src/main/java/com/example/demo/HelloController.java
@@ -1,13 +1,26 @@
 package com.example.demo;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.time.LocalDateTime;
+//hello world sssssssssssssssss
 @RestController
 public class HelloController {
 
     @GetMapping("/hello")
     public String sayHello() {
         return "Hello World from Backend!";
+    }
+
+    @GetMapping("/time")
+    public String getTime() {
+        return "Current server time is: " + LocalDateTime.now();
+    }
+
+    @GetMapping("/greet")
+    public String greetUser(@RequestParam(value = "name", defaultValue = "User") String name) {
+        return "Hello, " + name + "!";
     }
 }
