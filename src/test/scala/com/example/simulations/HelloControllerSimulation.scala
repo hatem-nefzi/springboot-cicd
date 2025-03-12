fad86776e135e03b72e09fa4465a4cba2ce05591
hatem-nefzi <hatem.nefzibouali@ensi-uma.tn>
2025-03-12 22:20:46 +0100

    corrected syntax in scala file

--- src/test/scala/com/example/simulations/HelloControllerSimulation.scala
+++ src/test/scala/com/example/simulations/HelloControllerSimulation.scala
@@ -8,7 +8,6 @@ class HelloControllerSimulation extends Simulation {
 
   val httpProtocol = http
     .baseUrl("http://192.168.1.2:8081") //change this back to "http://192.168.1.16:8081" when on site
-    }
     .acceptHeader("application/json")
 
   val scn = scenario("HelloControllerSimulation")
@@ -32,7 +31,7 @@ class HelloControllerSimulation extends Simulation {
 
   setUp(
     scn.inject(
-      atOnceUsers(20) // Simulate 10 users at once
+      atOnceUsers(20) // Simulate 20 users at once
     )
   ).protocols(httpProtocol)
 }
